59caf1682fab23e207f64caf302a90d4560e7dec
SteveKeenan <62963840+SteveKeenan@users.noreply.github.com>
2020-06-17 16:28:56 -0600

    basic auth setup

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -11,4 +14,20 @@ def home(request):
 
 # Auth routes
 def signup(request):
-    return render(request, 'signup.html')
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            error_message = "Invalid signup please try again"
+    else:
+        form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
+
+
+def login(request):
+    return render(request, 'registration/login.html')

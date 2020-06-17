f9250cb002951bcd5c70c92a9f15ab8f463da80e
SteveKeenan <62963840+SteveKeenan@users.noreply.github.com>
2020-06-17 16:05:22 -0600

    beginning auth setup

--- wayfarer/urls.py
+++ wayfarer/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import path, include
 from django.contrib import admin
 from django.urls import path
 
+# Add the include function to the import
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # In this case '' represents the root route
+    path('', include('main_app.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]

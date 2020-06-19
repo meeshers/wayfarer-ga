2b132dcc234d2018e7b7abd182a1ae359b8e799a
Michelle Tran <m.tran1223@gmail.com>
2020-06-19 12:18:18 -0700

    fixed models

--- main_app/models.py
+++ main_app/models.py
@@ -1,30 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import User
 import datetime
-from django.db.models.signals import post_save
-from django.dispatch import receiver
+#from django.db.models.signals import post_save
+#from django.dispatch import receiver
 
 # Create your models here.
-class City(models.Model):
-  name = models.CharField(max_length=50)
-  #post = models.ForeignKey(Post, on_delete = models.CASCADE)
-
-  def __str__(self):
-    return self.name
-
 class Post(models.Model):
   author = models.ForeignKey(User, on_delete = models.CASCADE)
-  city = models.ForeignKey(City, on_delete = models.CASCADE)
   title = models.CharField(max_length=100)
   content = models.TextField(max_length=250)
 
   def __str__(self):
     return f'Title: {self.title} in {self.city}'
 
+class City(models.Model):
+  name = models.CharField(max_length=50)
+  post = models.ForeignKey(Post, on_delete = models.CASCADE, null=True)
+
+  def __str__(self):
+    return self.name
+
+
 # extending User model to have city/date
 class Profile(models.Model):
   user = models.OneToOneField(User, on_delete=models.CASCADE)
-  post = models.ForeignKey(Post, on_delete = models.CASCADE)
   city = models.CharField(max_length=100)
   date = datetime.date
 
